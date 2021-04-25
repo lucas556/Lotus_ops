a14ba5577e979f012736a7f9ac21bdb11e530252
lucas556 <56923561@qq.com>
2021-04-26 03:14:23 +0800

    Create set.sh

---
+++ set.sh
@@ -0,0 +1,67 @@
+# 更新源
+  echo "更新源开始"
+  sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+  cat > /etc/apt/sources.list << EOF
+##阿里源
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+EOF
+
+apt-get update
+
+# 安装依赖
+sudo apt install -y make pkg-config mesa-opencl-icd ocl-icd-opencl-dev libclang-dev libhwloc-dev hwloc gcc git bzr jq tree openssh-server python3 cpufrequtils sysfsutils supervisor nfs-common
+
+# -----------------------------时钟校验------------------------------------------------
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+ntpdate ntp.aliyun.com
+
+# -----------------------------CPU性能模式------------------------------------------------
+sudo cpufreq-set -g performance     # 重启后无效;必须安装cpufrequtils
+echo "devices/system/cpu/cpu0/cpufreq/scaling_governor = performance" >> /etc/sysfs.conf  # 永久;必须安装sysfsutils
+
+# 设置 ulimit
+ulimit -n 1048576
+sed -i "/nofile/d" /etc/security/limits.conf
+echo "* hard nofile 1048576" >> /etc/security/limits.conf
+echo "* soft nofile 1048576" >> /etc/security/limits.conf
+echo "root hard nofile 1048576" >> /etc/security/limits.conf
+echo "root soft nofile 1048576" >> /etc/security/limits.conf
+
+# -----------------------------时钟校验结束------------------------------------------------
+
+#-----------------------------swappiness=1-----------------------------------------------------------------
+sysctl vm.swappiness=1
+sed -i "/swappiness/d" /etc/sysctl.conf
+echo "vm.swappiness=1" >> /etc/sysctl.conf
+
+# ----------------------------设置VM--------------------------------------------------
+sysctl vm.dirty_bytes=53687091200
+sed -i "/dirty_bytes/d" /etc/sysctl.conf
+echo "vm.dirty_bytes=53687091200" >> /etc/sysctl.conf
+
+sysctl vm.dirty_background_bytes=10737418240
+sed -i "/dirty_background_bytes/d" /etc/sysctl.conf
+echo "vm.dirty_background_bytes=10737418240" >> /etc/sysctl.conf
+
+sysctl vm.vfs_cache_pressure=1000
+sed -i "/vfs_cache_pressure/d" /etc/sysctl.conf
+echo "vm.vfs_cache_pressure=1000" >> /etc/sysctl.conf
+
+sysctl vm.dirty_writeback_centisecs=100
+sed -i "/dirty_writeback_centisecs/d" /etc/sysctl.conf
+echo "vm.dirty_writeback_centisecs=100" >> /etc/sysctl.conf
+
+sysctl vm.dirty_expire_centisecs=100
+sed -i "/dirty_expire_centisecs/d" /etc/sysctl.conf
+echo "vm.dirty_expire_centisecs=100" >> /etc/sysctl.conf
+
+# ----------------------------设置VM结束--------------------------------------------------
